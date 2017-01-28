11549c67963e45fb5771f6d3c9cd6397ce072ea7
KevinF15 <kevin.af09@gmail.com>
2017-01-28 04:11:54 +0000

    Refactorizar script

--- app.js
+++ app.js
@@ -1,18 +1,62 @@
 'use strict';
 
-const Web = require('node-static');
-const webPort = 8080;
-const path = new Web.Server('./web');
+const fs = require('fs');
+const http = require('http');
+const path = require('path');
 
-require('http').createServer(function(request, response) {
-    request.addListener('end', function () {
-        path.serve(request, response, function (err, result) {
-            if (err) {
-                console.error("WEB ERROR!: " + request.url + " - " + err.message);
- 
-                response.writeHead(err.status, err.headers);
-                response.end();
+http.createServer(function (request, response) {
+    
+    let filePath = '.' + request.url;
+    if (filePath == './') {
+        filePath = './web/index.html';
+    }
+
+    let extname = path.extname(filePath);
+    let contentType = 'text/html';
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;      
+        case '.jpg':
+            contentType = 'image/jpg';
+            break;
+        case '.wav':
+            contentType = 'audio/wav';
+            break;
+    }
+
+    fs.readFile(filePath, function(error, content) {
+        if (error) {
+            if(error.code == 'ENOENT'){
+                fs.readFile('./web/404.html', function(error, content) {
+                    response.writeHead(200, { 'Content-Type': contentType });
+                    response.end(content, 'utf-8');
+                });
             }
-        });
-    }).resume();
-}).listen(webPort);
+            else { // Otros errores
+                response.writeHead(500);
+                response.end('Ocurrio un error al intentar cargar la pagina!\n ' + error);
+                response.end(); 
+            }
+        }
+        else { // Flujo normal de la pagina
+            response.writeHead(200, { 'Content-Type': contentType });
+            response.end(content, 'utf-8');
+        }
+    });
+    
+}).listen(8080);
+
+console.log(
+    '\n',
+    '-------------- Init WebPage --------------'
+);
